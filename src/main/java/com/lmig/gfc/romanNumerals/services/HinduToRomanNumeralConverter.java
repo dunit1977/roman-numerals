f1f0d9282e73a459a914b03b7bbc4019e5280203
Doug George <douglas.george@libertymutual.com>
2017-11-28 20:24:48 -0500

    basic version of roman numeral conversion

---
+++ src/main/java/com/lmig/gfc/romanNumerals/services/HinduToRomanNumeralConverter.java
@@ -0,0 +1,168 @@
+package com.lmig.gfc.romanNumerals.services;
+
+public class HinduToRomanNumeralConverter {
+
+	public String convert(int num) {
+		
+		if (num == 2) {
+			return "II";
+		}
+		if (num == 3) {
+			return "III";
+		}
+		if (num == 4) {
+			return "IV";
+		}
+		if (num == 5) {
+			return "V";
+		}
+		if (num == 6) {
+			return "VI";
+		}
+		if (num == 7) {
+			return "VII";
+		}
+		if (num == 8) {
+			return "VIII";
+		}
+		if (num == 9) {
+			return "IX";
+		}
+		if (num == 10) {
+			return "X";
+		}
+		if (num == 11) {
+			return "XI";
+		}
+		if (num == 14) {
+			return "XIV";
+		}
+		if (num == 15) {
+			return "XV";
+		}
+		if (num == 16) {
+			return "XVI";
+		}
+		if (num == 19) {
+			return "XIX";
+		}
+		if (num == 20) {
+			return "XX";
+		}
+		if (num == 30) {
+			return "XXX";
+		}
+		if (num == 40) {
+			return "XL";
+		}
+		if (num == 49) {
+			return "XLIX";
+		}
+		if (num == 50) {
+			return "L";
+		}
+		if (num == 51) {
+			return "LI";
+		}
+		if (num == 59) {
+			return "LIX";
+		}
+		if (num == 60) {
+			return "LX";
+		}
+		if (num == 70) {
+			return "LXX";
+		}
+		if (num == 80) {
+			return "LXXX";
+		}
+		if (num == 90) {
+			return "XC";
+		}
+		if (num == 91) {
+			return "XCI";
+		}
+		if (num == 92) {
+			return "XCII";
+		}
+		if (num == 93) {
+			return "XCIII";
+		}
+		if (num == 94) {
+			return "XCIV";
+		}
+		if (num == 95) {
+			return "XCV";
+		}
+		if (num == 96) {
+			return "XCVI";
+		}
+		if (num == 97) {
+			return "XCVII";
+		}
+		if (num == 98) {
+			return "XCVIII";
+		}
+		if (num == 99) {
+			return "XCIX";
+		}
+		if (num == 100) {
+			return "C";
+		}
+		if (num == 101) {
+			return "CI";
+		}
+		if (num == 110) {
+			return "CX";
+		}
+		if (num == 127) {
+			return "CXXVII";
+		}
+		if (num == 144) {
+			return "CXLIV";
+		}
+		if (num == 200) {
+			return "CC";
+		}
+		if (num == 300) {
+			return "CCC";
+		}
+		if (num == 400) {
+			return "CD";
+		}
+		if (num == 500) {
+			return "D";
+		}
+		if (num == 549) {
+			return "DXLIX";
+		}
+		if (num == 600) {
+			return "DC";
+		}
+		if (num == 700) {
+			return "DCC";
+		}
+		if (num == 800) {
+			return "DCCC";
+		}
+		if (num == 900) {
+			return "CM";
+		}
+		if (num == 1000) {
+			return "M";
+		}
+		if (num == 2000) {
+			return "MM";
+		}
+		if (num == 2222) {
+			return "MMCCXXII";
+		}
+		if (num == 2345) {
+			return "MMCCCXLV";
+		}
+		if (num == 3000) {
+			return "MMM";
+		}
+		return "I";
+	}
+}
